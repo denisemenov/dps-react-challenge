c34338fda60b7c056436946e035a5976ff766937
Denis Semenov <hello@denisemenov.com>
2024-07-13 15:27:51 +0200

    Add tailwind.css Update some packages in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import dpsLogo from './assets/DPS.svg';
 import './App.css';
 
 function App() {
 	return (
 		<>
-			<div>
-				<a href="https://www.digitalproductschool.io/" target="_blank">
-					<img src={dpsLogo} className="logo" alt="DPS logo" />
-				</a>
-			</div>
-			<div className="home-card">
-				<p>Your solution goes here 😊</p>
-			</div>
+			<h1 className="text-3xl font-bold">Hello world!</h1>
 		</>
 	);
 }
